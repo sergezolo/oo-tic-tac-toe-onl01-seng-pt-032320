bf7c209cf15867e6c3f1ccf5ba9e20d4249488b3
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-16 23:21:52 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,79 @@
+class TicTacToe
+  
+  WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+  
+  def initialize
+    @board = Array.new(9, " ")
+  end
+ 
+  #def initialize(board = nil)
+  #  @board = board || Array.new(9, " ")
+  #end
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end 
+  
+  def input_to_index(input)
+    index = input.to_i - 1
+  end
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  def position_taken?(index)
+    !(@board[index] == " ")
+  end
+  
+  def valid_move?(index)
+    index.between?(0, 8) && !(position_taken?(index))
+  end
+  
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+    
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def turn
+    puts "1-9?"
+    input = gets
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def won?
+    WIN_COMBINATIONS.find do |win_combo|
+      @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[1]] == @board[win_combo[2]]
+    end
+  end
+  
+  def full?
+    !(@board.any? {|position| position == " "})
+  end
+  
+  def draw?
+    !won? && full?
+  end
+
+  
+
+ 
+ 
+
+ 
+
+  
+end
