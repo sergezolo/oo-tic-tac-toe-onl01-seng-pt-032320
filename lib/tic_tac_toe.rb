3e492fa1a846da72cbd8ca1b5997de3a02d657ab
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-17 01:57:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,7 @@ class TicTacToe
   
   def won?
     WIN_COMBINATIONS.find do |win_combo|
-      position_taken?(win_combo[0]) && @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[1]] == @board[win_combo[2]])
+      position_taken?(win_combo[0]) && @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[1]] == @board[win_combo[2]]
     end
   end
   
